b4d2f93c8dce852fb97598041813a7b67905605f
Christoph Lenovo <christophhedtrich@gmail.com>
2018-04-19 18:57:44 +0200

    update example

--- class3/wiki_example.py
+++ class3/wiki_example.py
@@ -21,6 +21,27 @@ soup.title.string
 # find all links
 soup.find_all('a')
 
+paragraphs = soup.find_all('p')
+# the fifth paragraph describes where he lives
+p5 = paragraphs[4].text 
+# count Guido
+p5.count("Guido")
+# count guido in all paragraphs
+nGuido = 0
+for p in paragraphs:
+    ptxt = p.text 
+    pcount = ptxt.count("Guido")
+    nGuido = nGuido + pcount # shorter notation: nGuido += pcount
+
+print("We found ", nGuido, " Guidos")
+
+# alternative with regex for exact matches
+import re
+count = sum(1 for _ in re.finditer(r'\b%s\b' % re.escape("Guido"), p5))
+# See: https://docs.python.org/3.6/howto/regex.html
+
+
+
 # find where Guido lives in a roundabout manner
 table = soup.find('table')
 for row in table.find_all('tr'):
@@ -30,14 +51,10 @@ for row in table.find_all('tr'):
         if th.string == "Residence":
             print("Guido lives in: ", row.find('a').string)
 
-paragraphs = soup.find_all('p')
 
-# the fifth paragraph describes where he lives
-p5 = paragraphs[4].text 
-# count Guido
-p5.count("Guido")
 
-# alternative with regex for exact matches
-import re
-count = sum(1 for _ in re.finditer(r'\b%s\b' % re.escape("Guido"), p5))
-# See: https://docs.python.org/3.6/howto/regex.html
+
+
+
+
+
